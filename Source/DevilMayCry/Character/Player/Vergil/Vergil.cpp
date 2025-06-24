c0341215b68d458afbdba8acf007b63e790d0e7a
동찬 황 <shidge98@gmail.com>
2025-06-24 11:22:17 +0900

    황동찬 : 서버 테스트 업데이트 TempScene에서 가능 1번이 호스트 2번이 참여 위치동기화 오류 존재

---
+++ Source/DevilMayCry/Character/Player/Vergil/Vergil.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Vergil.h"
+
+AVergil::AVergil()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AVergil::BeginPlay()
+{
+	Super::BeginPlay();
+	USkeletalMesh* SKM = LoadObject<USkeletalMesh>(nullptr, TEXT("/Script/Engine.SkeletalMesh'/Game/Asset/Character/Player/Vergil/Mesh/Vergil.Vergil'"));
+
+	if (SKM)
+	{
+		GetMesh()->SetSkeletalMesh(SKM);
+		GetMesh()->SetRelativeLocation(FVector(0.f, 0.f, -90.f));
+		GetMesh()->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+	}
+}
+
+void AVergil::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AVergil::LeftClick()
+{
+}
+
+void AVergil::RightClick()
+{
+}
+
+void AVergil::WheelClick()
+{
+}
+
+void AVergil::EKey()
+{
+}
+
+void AVergil::ShiftKey()
+{
+}
+
+void AVergil::SpaceKey()
+{
+}
